1a36c8255690d50594e7ddfa5d5ded4546ac145d
Marissa Zhong <mwz2101@columbia.edu>
2018-07-15 19:54:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(num) {
+  if (num <= 400) {
+    return 'This one is on me!';
+  } else if (num > 2500) {
+    return 'No can do.';
+  } else if (num > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip_type) {
+  switch (tip_type) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
