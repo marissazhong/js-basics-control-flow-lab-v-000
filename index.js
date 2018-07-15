1033eea7f4a9fab77032c3fe24984420a582d643
Marissa Zhong <mwz2101@columbia.edu>
2018-07-15 19:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(num) {
+  if num <= 400 {
+    return 'This one is on me!';
+  } else if num > 2500 {
+    return 'No can do.';
+  } else if num > 2000 {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
