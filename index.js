ca2799fe3da9b59776f279de9577c7677fc6e02f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-15 19:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function scuberGreetingForFeet(num) {
 }
 
 function ternaryCheckCity(city) {
-  city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
